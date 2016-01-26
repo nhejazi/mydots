d900952c0f68209126d424e04a9e311586f34137
Nima Hejazi <nimahejazi@Nimas-MacBook-4.local>
2016-01-26 12:26:03 -0800

    overhaul, changes all around

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-## MyDotfiles ~ easy access to shell customization files
+# MyDotfiles ~ easy access to shell customization files
 
 Collection of configuration files to set OS X and Ubuntu to my preferences.
 
+## Directions/Guide
+```bash
+git clone https://github.com/nhejazi/mydotfiles.git
+cd mydotfiles
+sh _setup.sh
+```
---
+++ _setup.sh
@@ -0,0 +1,14 @@
+# generate symlinks to important files in home directory
+
+cd
+
+# for terminal (bash) customization
+ln -s ~/git-repos/mydotfiles/bashrc ~/.bashrc
+ln -s ~/git-repos/mydotfiles/bash_profile ~/.bash_profile
+ln -s ~/git-repos/mydotfiles/bash_color ~/.bash_color
+
+# for git configuration/customization
+ln -s ~/git-repos/mydotfiles/git-prompt.sh ~/.git-prompt.sh
+ln -s ~/git-repos/mydotfiles/gitconfig ~/.gitconfig
+ln -s ~/git-repos/mydotfiles/gitignore_global ~/.gitignore_global
+ln -s ~/git-repos/mydotfiles/gitmessage.txt ~/.gitmessage.txt
