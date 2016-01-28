07bf9cc4ce9e05b659602c85367be3b0faac57a5
Nima Hejazi <nima.hejazi7@gmail.com>
2016-01-27 21:21:17 -0800

    fix syntax in setup

--- _setup.sh
+++ _setup.sh
@@ -3,53 +3,46 @@
 cd
 
 # for shell (bash) customization
-if [ -e ~/.bashrc ]
-  then
-    echo "bashrc dotfile exists - will be removed."
-    rm -f ~/.bashrc
+if [ -e ~/.bashrc ]; then
+    echo "bashrc dotfile exists - will be removed.";
+    rm -f ~/.bashrc;
 fi
 ln -s ~/git-repos/mydotfiles/bashrc ~/.bashrc
 
-if [ -e ~/.bash_profile ]
-  then
-    echo "bash_profile dotfile exists - will be removed."
-    rm -f ~/.bash_profile
+if [ -e ~/.bash_profile ]; then
+    echo "bash_profile dotfile exists - will be removed.";
+    rm -f ~/.bash_profile;
 fi
 ln -s ~/git-repos/mydotfiles/bash_profile ~/.bash_profile
 
-if [ -e ~/.bash_color ]
-  then
-    echo "bash_color dotfile exists - will be removed."
-    rm -f ~/.bash_color
+if [ -e ~/.bash_color ]; then
+    echo "bash_color dotfile exists - will be removed.";
+    rm -f ~/.bash_color;
 fi
 ln -s ~/git-repos/mydotfiles/bash_color ~/.bash_color
 
 
 # for git configuration and customization
-if [ -e ~/.git-prompt.sh ]
-  then
-    echo "git-prompt dotfile exists - will be removed."
-    rm -f ~/.git-prompt.sh
+if [ -e ~/.git-prompt.sh ]; then
+    echo "git-prompt dotfile exists - will be removed.";
+    rm -f ~/.git-prompt.sh;
 fi
 ln -s ~/git-repos/mydotfiles/git-prompt.sh ~/.git-prompt.sh
 
-if [ -e ~/.gitconfig ]
-  then
-    echo "gitconfig dotfile exists - will be removed."
-    rm -f ~/.gitconfig
+if [ -e ~/.gitconfig ]; then
+    echo "gitconfig dotfile exists - will be removed.";
+    rm -f ~/.gitconfig;
 fi
 ln -s ~/git-repos/mydotfiles/gitconfig ~/.gitconfig
 
-if [ -e ~/.gitignore_global ]
-  then
-    echo "gitignore dotfile exists - will be removed."
-    rm -f ~/.gitignore_global
+if [ -e ~/.gitignore_global ]; then
+    echo "gitignore dotfile exists - will be removed.";
+    rm -f ~/.gitignore_global;
 fi
 ln -s ~/git-repos/mydotfiles/gitignore_global ~/.gitignore_global
 
-if [ -e ~/.gitmessage.txt ]
-  then
-    echo "gitmessage dotfile exists - will be removed."
-    rm -f ~/.gitmessage.txt
+if [ -e ~/.gitmessage.txt ]; then
+    echo "gitmessage dotfile exists - will be removed.";
+    rm -f ~/.gitmessage.txt;
 fi
 ln -s ~/git-repos/mydotfiles/gitmessage.txt ~/.gitmessage.txt
