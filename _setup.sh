2324b65d542d5ed460e1382433ee0e8f0b8a5e91
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-29 23:15:08 -0800

    consolidate paths

--- _setup.sh
+++ _setup.sh
@@ -8,19 +8,19 @@ if [ -e ~/.bashrc ]; then
     echo "bashrc dotfile exists - will be removed.";
     rm -f ~/.bashrc;
 fi
-ln -s ~/git-repos/mydotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/bashrc ~/.bashrc
 
 if [ -e ~/.bash_profile ]; then
     echo "bash_profile dotfile exists - will be removed.";
     rm -f ~/.bash_profile;
 fi
-ln -s ~/git-repos/mydotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
 
 if [ -e ~/.bash_color ]; then
     echo "bash_color dotfile exists - will be removed.";
     rm -f ~/.bash_color;
 fi
-ln -s ~/git-repos/mydotfiles/bash_color ~/.bash_color
+ln -s ~/dotfiles/bash_color ~/.bash_color
 
 
 # for git configuration and customization
@@ -28,22 +28,22 @@ if [ -e ~/.git-prompt.sh ]; then
     echo "git-prompt dotfile exists - will be removed.";
     rm -f ~/.git-prompt.sh;
 fi
-ln -s ~/git-repos/mydotfiles/git-prompt.sh ~/.git-prompt.sh
+ln -s ~/dotfiles/git-prompt.sh ~/.git-prompt.sh
 
 if [ -e ~/.gitconfig ]; then
     echo "gitconfig dotfile exists - will be removed.";
     rm -f ~/.gitconfig;
 fi
-ln -s ~/git-repos/mydotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
 
 if [ -e ~/.gitignore_global ]; then
     echo "gitignore dotfile exists - will be removed.";
     rm -f ~/.gitignore_global;
 fi
-ln -s ~/git-repos/mydotfiles/gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
 
 if [ -e ~/.gitmessage.txt ]; then
     echo "gitmessage dotfile exists - will be removed.";
     rm -f ~/.gitmessage.txt;
 fi
-ln -s ~/git-repos/mydotfiles/gitmessage.txt ~/.gitmessage.txt
+ln -s ~/dotfiles/gitmessage.txt ~/.gitmessage.txt
